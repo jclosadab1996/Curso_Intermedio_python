2a7400e63b5043629b9b195e2dcfcfa52ceb0481
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-06-19 16:29:32 -0500

    Add implementation for eliminating duplicate elements from sets

---
+++ 04-elimina-elementos-duplicados.py
@@ -0,0 +1,9 @@
+countries = {"MX", "COL", "ARG", "USA"}
+northAm = {"USA", "CANADA"}
+centralAm = {"MX", "GT", "BZ"}
+southAm = {"COL", "BZ", "ARG"}
+
+new_set = set()
+# Escribe tu solución 👇
+new_set = countries | northAm  | centralAm | southAm
+print(new_set)
